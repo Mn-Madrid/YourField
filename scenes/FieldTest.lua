dbe09d448a1c8cfc846ac34474fc2ad9a49657d5
rust <juliomontero.mostoles@gmail.com>
2026-08-22 01:00:01 +0200

    Remaking the textbox

--- scenes/FieldTest.lua
+++ scenes/FieldTest.lua
@@ -1,31 +1,22 @@
 local FieldTest ={}
 
-local debugTB = GLT.textbox.new({"ALOYSSE", "ÑINGALING"}, "B", 1, 35, "JOHAN")
+local TEST = GLT.textbox
+
 
 function FieldTest:load()
+  
 end
 
 function FieldTest:update(dt)
-  debugTB:update(dt)
-  if debugTB:OPTIOND() == 1 then
-    debugTB.OPTION = 0
-    debugTB:setOut()
-    debugTB:setIn({"WHATS UP BRO", "HOW IS IT HANGIN"}, "A", 0, "")
-  elseif debugTB:OPTIOND() == 2 then
-    debugTB.OPTION = 0
-    debugTB:setOut()
-  end
+
 end
 
 function FieldTest.keypressed(key)
-  debugTB:keypressed(key)
-  if key == "a" then
-    debugTB:setIn({"AWAWA", "LOREM IPÑUM"}, "B", 0)
-  end
+
 end
 
 function FieldTest:draw()
-  debugTB:draw()
+
 end
 
 return FieldTest
